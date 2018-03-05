6760a1257544281c84b048740a0c0017b733a71d
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-05 00:34:31 -0600

    Initial commit

---
+++ EulerProblems/Euler_001.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+/// <summary>
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+/// The sum of these multiples is 23. 
+/// Find the sum of all the multiples of 3 or 5 below 1000.
+/// </summary>
+namespace EulerProblems
+{
+  public class Euler_001
+  {
+    internal void start()
+    {
+      WriteLine("Hello there");
+    }
+  }
+}
---
+++ EulerProblems/Home.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Threading;
+
+namespace EulerProblems
+{
+  public class Home
+  {
+    static void Main(string[] args)
+    {
+      //Thread thread = new Thread()
+      new Euler_001().start();
+    }
+  }
+}
