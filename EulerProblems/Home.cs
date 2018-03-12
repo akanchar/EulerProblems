bf691eb8229a9f4207ef47cbb38525f50b0cb90d
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-11 20:02:52 -0500

    MOD: Minor changes

--- EulerProblems/Home.cs
+++ EulerProblems/Home.cs
@@ -1,11 +1,14 @@
-﻿namespace EulerProblems
+﻿using System;
+using static System.Console;
+namespace EulerProblems
 {
-  public class Home
-  {
-    static void Main(string[] args)
+    public class Home
     {
-      //Thread thread = new Thread()
-      new Euler_001().start();
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            new Euler_005().start();
+            WriteLine("Execution time - " + ((long)(DateTime.Now.Ticks) - (long)dateTime.Ticks)/10000 + " milliseconds.");
+        }
     }
-  }
 }
