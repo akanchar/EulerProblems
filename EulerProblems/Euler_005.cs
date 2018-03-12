c934cd2c2ced842fd88077f90b1653a427c3fbac
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-11 20:13:55 -0500

    MOD: Implementing faster algorithm
    Reducing the number of loops, by addding prime numbers first and minimum numbers needed in the 'if' condition. Helps shirt curcuit better.

--- EulerProblems/Euler_005.cs
+++ EulerProblems/Euler_005.cs
@@ -3,6 +3,8 @@ using static System.Console;
 /// <summary>
 /// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 /// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+/// Runs faster because the prime numbers are listed first in the 'if' condition, which short cirtcuits 
+/// the 'if' loop faster if not the right number.
 /// </summary>
 namespace EulerProblems
 {
@@ -15,18 +17,16 @@ namespace EulerProblems
             ArrayList uniqueNumbers = new ArrayList();
             for (int i = finalFactor; numberFound; i++)
             {
-                for (int j = 2; j <= finalFactor; j++)
-                {
-                    if (i % j != 0)
-                        numberFound = false;
-                }
-                if (numberFound)
+                if (i % 19 == 0 && i % 17 == 0 &&
+                i % 13 == 0 && i % 11 == 0 &&
+                i % 20 == 0 && i % 18 == 0 &&
+                i % 16 == 0 && i % 15 == 0 &&
+                i % 14 == 0 && i % 20 == 0)
                 {
                     WriteLine("The smallest number that can be divisble by each of the numbers from 1 to " + finalFactor +
                 " is " + i);
                     break;
                 }
-                numberFound = true;
             }
         }
     }
